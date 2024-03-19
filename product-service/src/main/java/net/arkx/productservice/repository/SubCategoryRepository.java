413f647b7820d938196b8dc40982bf6456dd811f
SoukainaAitoussous <soukaina.aitoussous@cloudlink.us>
2024-03-19 15:14:05 +0000

    changement au niveau de subcategory repo

--- product-service/src/main/java/net/arkx/productservice/repository/SubCategoryRepository.java
+++ product-service/src/main/java/net/arkx/productservice/repository/SubCategoryRepository.java
@@ -1,8 +1,9 @@
 package net.arkx.productservice.repository;
 
+import net.arkx.productservice.entities.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubCategoryRepository extends JpaRepository<SubCategoryRepository, Long> {
+public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
 }
