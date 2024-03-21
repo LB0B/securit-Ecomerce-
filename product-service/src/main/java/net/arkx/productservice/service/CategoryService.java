69d5979cb358781c10f8c10493b65099fd4b4327
SoukainaAitoussous <soukaina.aitoussous@cloudlink.us>
2024-03-21 11:01:26 +0000

    Add CRUD & SEARCH method of category

--- product-service/src/main/java/net/arkx/productservice/service/CategoryService.java
+++ product-service/src/main/java/net/arkx/productservice/service/CategoryService.java
@@ -1,12 +1,68 @@
 package net.arkx.productservice.service;
 
+import lombok.Getter;
+import net.arkx.productservice.entities.Category;
+import net.arkx.productservice.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class CategoryService {
-    private final net.arkx.productservice.repository.CategoryRepository categoryRepository;
+    //private final net.arkx.productservice.repository.CategoryRepository categoryRepository;
+    //public CategoryService(net.arkx.productservice.repository.CategoryRepository categoryRepository) {
+       //this.categoryRepository = categoryRepository;
 
-    public CategoryService(net.arkx.productservice.repository.CategoryRepository categoryRepository) {
+//***********************************DEPENDENCY INJECTION*************************************************
+    private final CategoryRepository categoryRepository;
+    public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+//***************************************CRUD METHODS**********************************************
+//CREATE METHOD:
+   public void createCategory(Category category){
+        categoryRepository.save(category);
+   }
+//RETRIEVE METHOD:
+    public List<Category> getCategory(){
+       return categoryRepository.findAll();
+    }
+//DELETE METHOD:
+    public void deleteCategoryById(Long id){
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+        }
+    }
+//UPDATE METHOD:
+    public void updateCategory(Long id,Category category){
+        if(categoryRepository.findById(id).isPresent()) {
+            Category categoryToUpdate = categoryRepository.findById(id).get();
+            categoryToUpdate.setName(category.getName());
+            categoryRepository.save(categoryToUpdate);
+        }
+    }
+//***************************************SEARCH METHODS**********************************************
+//SEARCH BY ID:
+    public Category searchCategoryById(Long id){
+        return categoryRepository.findById(id).get();
+    }
+//SEARCH BY NAME:
+    //Category catToSearch;
+//    public Category searchCategoryByName(String nameToSearch){
+//        List<Category> categoryList=getCategory();
+//        for(Category category:categoryList){
+//            if(category.getName().equalsIgnoreCase(nameToSearch)){
+//                Long id=category.getId();
+//                catToSearch=searchCategoryById(id);
+//            }
+//        }
+//        return catToSearch;
+//    }
+    public Category searchCategoryByName(String name){
+        return categoryRepository.findByNameIgnoreCase(name);
+    }
 }
+
--- product-service/src/main/java/net/arkx/productservice/web/CategoryController.java
+++ product-service/src/main/java/net/arkx/productservice/web/CategoryController.java
@@ -1,13 +1,58 @@
 package net.arkx.productservice.web;
 
+import jakarta.persistence.EntityNotFoundException;
+import net.arkx.productservice.entities.Category;
 import net.arkx.productservice.service.CategoryService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("category")
 public class CategoryController {
+    //***********************************DEPENDENCY INJECTION*************************************************
     private final CategoryService categoryService;
 
     public CategoryController(CategoryService categoryService) {
         this.categoryService = categoryService;
     }
+
+    //***********************************POST METHOD******************************************************
+    @PostMapping("/create")
+    public void addCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+    }
+
+    //***********************************GET METHOD******************************************************
+    @GetMapping
+    public List<Category> getAllCaategories() {
+        return categoryService.getCategory();
+    }
+    @GetMapping("{id}")
+    public Category getCategoryById(@PathVariable Long id){
+        return categoryService.searchCategoryById(id);
+    }
+    @GetMapping("/{name}")
+    public Category getCategoryByName(@RequestParam String name){
+        return categoryService.searchCategoryByName(name);
+    }
+
+    //***********************************DELETE METHOD******************************************************
+    @DeleteMapping("{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategoryById(id);
+    }
+
+    //***********************************UPDATE METHOD******************************************************
+    @PutMapping("{id}")
+    public void updateCategory(
+            @PathVariable Long id,
+            @RequestBody Category category
+    )
+        throws EntityNotFoundException{
+        categoryService.updateCategory(id,category);
+    }
+
 }
+
+
