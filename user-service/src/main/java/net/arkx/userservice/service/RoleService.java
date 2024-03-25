a71b648359b321d381754e7dba56601aa37dd7b0
Ibrahim Ayoub Meskine <126199620+LB0B@users.noreply.github.com>
2024-03-25 23:20:42 +0000

    Add Crud Role

--- user-service/src/main/java/net/arkx/userservice/service/RoleService.java
+++ user-service/src/main/java/net/arkx/userservice/service/RoleService.java
@@ -2,8 +2,11 @@ package net.arkx.userservice.service;
 
 import net.arkx.userservice.entities.Role;
 import net.arkx.userservice.entities.User;
+import net.arkx.userservice.exceptions.RoleExceptions.RoleNotFoundException;
 import net.arkx.userservice.repository.RoleRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoleService {
@@ -15,9 +18,39 @@ public class RoleService {
 
 
     // Create Role
-
     public Role createRole(Role newRole){
         return roleRepository.save(newRole);
+    }
+    //Read all Roles
+    public List<Role> getAllRoles(){
+        return roleRepository.findAll();
+    }
+    //Read Role by id
+    public Role getRoleById(Long id){
+        Role role = roleRepository.findById(id).orElse(null);
+        if(role != null){
+            return role;
+        }else {
+            throw new RoleNotFoundException("Can't find this Role with Id: "+id);
+        }
+    }
+    //Read Role by name
+    public Role getRoleByName(String name){
+        return roleRepository.findByName(name);
+    }
+    // Update Role
+
+    //Delete Role by name
+    public void deleteRole(String name){
+        Role role = roleRepository.findByName(name);
+        roleRepository.delete(role);
+    }
+    //Delete Role by id
+    public void deleteRoleById(Long id){
+        Role role = roleRepository.findById(id).orElse(null);
+        if(role!=null) {
+            roleRepository.delete(role);
+        }
     }
 
 }
